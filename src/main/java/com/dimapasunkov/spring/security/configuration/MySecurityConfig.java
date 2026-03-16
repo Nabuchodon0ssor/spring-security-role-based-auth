5b16c70b4bebfb80197da4a93cb8db79b3706d5a
Dimitri Pasunkov <javarusher01@gmail.com>
2026-03-16 16:14:19 +0100

    Initial commit: Spring Security role-based authentication demo

---
+++ src/main/java/com/dimapasunkov/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,40 @@
+package com.dimapasunkov.spring.security.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+/*        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("emp").password("emp").roles("EMPLOYEE"))
+                .withUser(userBuilder.username("hr").password("hr").roles("HR"))
+                .withUser(userBuilder.username("boss").password("boss").roles("MANAGER", "HR"));*/
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE","MANAGER","HR")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+}
